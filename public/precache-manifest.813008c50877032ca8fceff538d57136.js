af1725ecebb2a1e56028643e4df36cc6e0eacbcf
nanwu <yuehnan.wu@cronumax.com>
2020-07-17 14:24:07 +0800

    first deployment

---
+++ public/precache-manifest.813008c50877032ca8fceff538d57136.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d96fba789319d323c2f7d21d4f5f2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0fde69d2ba4fcd7c1ad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "260d135e8d8b717bf145",
+    "url": "/static/js/2.9598ca2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9598ca2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0fde69d2ba4fcd7c1ad",
+    "url": "/static/js/main.add784bf.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
