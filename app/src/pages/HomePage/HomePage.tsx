9957dad1e397ee58eadc93ac532865fcf8c28fc3
nanwu <yuehnan.wu@cronumax.com>
2020-07-20 19:06:00 +0800

    Adjustment

--- app/src/pages/HomePage/HomePage.tsx
+++ app/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import SearchInputField from 'components/SearchInputField';
 
 export default function HomePage() {
-  return <React.Fragment>HOME</React.Fragment>;
+  return (
+    <div>
+      <SearchInputField></SearchInputField>
+    </div>
+  );
 }
