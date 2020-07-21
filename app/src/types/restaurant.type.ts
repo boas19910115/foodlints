f281d983ee684c6ccf87dd08f56b9b93c03da213
nanwu <yuehnan.wu@cronumax.com>
2020-07-21 18:54:01 +0800

    Define restaurant and restaurant open time

---
+++ app/src/types/restaurant.type.ts
@@ -0,0 +1,15 @@
+export type RestaurantName = string;
+
+export interface Restaurant {
+  name: RestaurantName;
+  openTimeList: string[];
+}
+
+export interface OpenTime {
+  duration: number;
+  endTime: string;
+  restaurantId: string;
+  restaurantName: RestaurantName;
+  startTime: string;
+  weekdayList: number[];
+}
