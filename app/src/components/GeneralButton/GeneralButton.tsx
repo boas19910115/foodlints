54be2f59ed9007af4872f615c3397c5a690e45fe
nanwu <yuehnan.wu@cronumax.com>
2020-07-21 18:50:08 +0800

    Created a general button based on boostrap button

---
+++ app/src/components/GeneralButton/GeneralButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, ButtonProps } from 'react-bootstrap';
+import classes from './GeneralButton.module.scss';
+import classnames from 'classnames';
+
+export default function GeneralButton(props: ButtonProps) {
+  const { className, children, ...rest } = props;
+  return (
+    <Button className={classnames(classes.GeneralButton, className)} {...rest}>
+      {children}
+    </Button>
+  );
+}
