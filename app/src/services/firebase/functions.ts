3357d7c995ce857537a81f7bc8d9a89b662bab57
nanwu <yuehnan.wu@cronumax.com>
2020-07-21 18:56:50 +0800

    Added function to query restaurant info (Client side)

--- app/src/services/firebase/functions.ts
+++ app/src/services/firebase/functions.ts
@@ -2,10 +2,18 @@ import { fireFunctions } from './init';
 
 interface FireFunctions {
   getAllRestaurantNames: firebase.functions.HttpsCallable;
+  getRestaurant: (
+    where: string,
+    is: string
+  ) => Promise<firebase.functions.HttpsCallableResult>;
 }
 
 const backendFunctions: FireFunctions = {
   getAllRestaurantNames: fireFunctions.httpsCallable('allRestaurantNames'),
+  getRestaurant: (where, is) => {
+    const getRestaurant = fireFunctions.httpsCallable(`restaurant`);
+    return getRestaurant({ where, is });
+  },
 };
 
 export default backendFunctions;
