e82c9e3190eccfd89e4e38373c7e30a84b0bab37
nanwu <yuehnan.wu@cronumax.com>
2020-07-21 18:52:11 +0800

    Created login page and member page

---
+++ app/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+// import classes from './LoginPage.module.scss';
+
+export default function LoginPage() {
+  return <React.Fragment>LoginPage</React.Fragment>;
+}
---
+++ app/src/pages/MemberPage/MemberPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+// import classes from './MemberPage.module.scss';
+
+export default function MemberPage() {
+  return <React.Fragment>MemberPage</React.Fragment>;
+}
