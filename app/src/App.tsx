e6e87d324657863b34c4ef7a7ae8ca3a8a7e5a22
nanwu <yuehnan.wu@cronumax.com>
2020-07-21 18:54:35 +0800

    Setup route

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import classes from './App.module.scss';
-import HomePage from 'pages/HomePage';
 import DefaultLayout from 'layouts/DefaultLayout';
+import { Switch, Route } from 'react-router-dom';
+import routeMap from 'routes/routeMap';
 
 function App() {
   return (
     <div className={classes.App}>
-      <DefaultLayout>
-        <HomePage></HomePage>
-      </DefaultLayout>
+      <Switch>
+        {Object.values(routeMap).map((r) => (
+          <Route exact key={r.name} path={r.path}>
+            <DefaultLayout>
+              <r.PageComponent></r.PageComponent>
+            </DefaultLayout>
+          </Route>
+        ))}
+      </Switch>
     </div>
   );
 }
