1f1c21ce6a57d018b1168a270f7e34185bb85e13
nanwu <yuehnan.wu@cronumax.com>
2020-07-23 19:39:04 +0800

    Added functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -51,7 +51,7 @@ export const helloWorld = httpsHandler((request, response) => {
 
 export const allRestaurantNames = httpsHandler(async (_, response) => {
   const doc = await firestore.collection('restaurant').doc('INFO').get();
-  const data = await doc.data();
+  const data = doc.data();
   functions.logger.info(data, { structuredData: true });
   response.send({ data });
 });
@@ -67,10 +67,45 @@ export const restaurant = httpsHandler(async (requset, response) => {
     .collection('restaurant')
     .where(where.toString(), '==', is)
     .get();
-  if (query.docs[0]) {
+  try {
     const data = query.docs[0].data();
-    response.send({ data });
-  } else {
+    response.send({ data: { ...data, id: query.docs[0].id } });
+  } catch (error) {
     throw Error(ErrorCodeEnum.NOT_FOUND_DATA);
   }
+});
+
+// export const restaurantByDateTime = httpsHandler(
+//   async (request, response) => {}
+// );
+
+export const openTimeListByRestaurantId = httpsHandler(
+  async (request, response) => {
+    const {
+      data: { restaurantId },
+    } = request.body;
+    const res = await firestore
+      .collection('opentime')
+      .where('restaurantId', '==', restaurantId)
+      .get();
+
+    const list = res.docs.map((d) => d.data());
+    response.send({ data: list });
+  }
+);
+
+// export const addCollection = httpsHandler(async (request, response) => {});
+
+// export const addFavorite = httpsHandler(async (request, response) => {});
+
+export const addMember = httpsHandler(async (request, response) => {
+  const {
+    data: { email, name, photoURL, favoriteCollections = [] },
+  } = request.body;
+  const res = await firestore.collection('user').doc(email).set({
+    name,
+    photoURL,
+    favoriteCollections,
+  });
+  response.send({ data: res });
 });
