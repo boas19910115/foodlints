f4c767ef552e4d87305ae8c7b6a54c3de5488550
yuehnanwu <joram19910115@gmail.com>
2020-07-23 14:13:44 +0800

    Added a function to check if the user signed in

--- app/src/hooks/useAuthManager.ts
+++ app/src/hooks/useAuthManager.ts
@@ -1,13 +1,16 @@
 import { useCallback } from 'react'
 import { googleSignin as serviceGoogleSigin } from 'services/firebase/auth/google'
 import { facebookSignin as serviceFacebookSignin } from 'services/firebase/auth/facebook'
+import { fireApp } from 'services/firebase/init'
 
 export default function useAuthManager() {
   const googleSignin = useCallback(serviceGoogleSigin, [])
   const facebookSignin = useCallback(serviceFacebookSignin, [])
+  const isSignedIn = useCallback(() => fireApp.auth().currentUser, [])
 
   return {
     googleSignin,
     facebookSignin,
+    isSignedIn,
   }
 }
