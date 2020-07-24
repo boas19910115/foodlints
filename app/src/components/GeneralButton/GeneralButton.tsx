e8414e9a6bf89675084bb6cf5b7fee6f746dbfcc
nanwu <yuehnan.wu@cronumax.com>
2020-07-24 11:45:23 +0800

    Make `GeneralButton` flexable

--- app/src/components/GeneralButton/GeneralButton.tsx
+++ app/src/components/GeneralButton/GeneralButton.tsx
@@ -3,8 +3,33 @@ import { Button, ButtonProps } from 'react-bootstrap'
 import classes from './GeneralButton.module.scss'
 import classnames from 'classnames'
 
-export default function GeneralButton(props: ButtonProps) {
-  const { className, children, ...rest } = props
+interface GeneralButtonProps extends ButtonProps {
+  CompType?: React.FunctionComponent<ButtonProps> | keyof HTMLElementTagNameMap
+}
+
+export default function GeneralButton(props: GeneralButtonProps) {
+  const { className, children, CompType, ...rest } = props
+  if (CompType) {
+    if (typeof CompType === 'string') {
+      return React.createElement(
+        CompType,
+        {
+          className: classnames(classes.GeneralButton, className, 'btn'),
+          ...rest,
+        },
+        children
+      )
+    } else {
+      return (
+        <CompType
+          className={classnames(classes.GeneralButton, className, 'btn')}
+          {...rest}
+        >
+          {children}
+        </CompType>
+      )
+    }
+  }
   return (
     <Button className={classnames(classes.GeneralButton, className)} {...rest}>
       {children}
